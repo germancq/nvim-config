89a4942b65a4b6ebba664da8e14375557cfabd49
germancq <germancq@gmail.com>
2024-07-30 00:30:21 +0200

    wip

---
+++ lua/plugins/formatter.lua
@@ -0,0 +1,40 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				javascriptreact = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				svelte = { "prettierd" },
+				css = { "prettierd" },
+				html = { "prettierd" },
+				json = { "prettierd" },
+				yaml = { "prettierd" },
+				markdown = { "prettierd" },
+				graphql = { "prettierd" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				systemverilog = { "verible" },
+				verilog = { "verible" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+}
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,49 @@
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+
+		local mason_tool_installer = require("mason-tool-installer")
+		local mason_lspconfig = require("mason-lspconfig")
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettierd", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint", -- python linter
+				"verible", --verilog and systemVerilog formatter
+			},
+		})
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"pyright",
+				"verible",
+				"lua_ls",
+				"marksman",
+				"pylsp",
+				"texlab",
+				"arduino_language_server",
+				"ansiblels",
+				"docker_compose_language_service",
+				"jsonls",
+				"yamlls",
+			},
+		})
+	end,
+}
